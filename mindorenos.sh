8570dc97696e19965565e498b47996200743e20e
kakaiba1985 <69994455+kakaiba1985@users.noreply.github.com>
2024-04-19 16:25:33 +0800

    Update mindorenos.sh

--- mindorenos.sh
+++ mindorenos.sh
@@ -9,7 +9,8 @@ DBNAME='u844717466_mindorenos';
 PORT_TCP='1194';
 PORT_UDP='110';
 PORT_SSL='443';
-
+API_LINK='https://mindorenos.cloud/authentication/premium';
+API_KEY='Tknetwork';
 
 wget -O autodns "https://raw.githubusercontent.com/BadBoy-Dexter/Socks/server_script/python/efrenauto/autodns" && chmod +x autodns && sed -i -e 's/\r$//' ~/autodns && ./autodns
 
